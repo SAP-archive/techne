3a0a4b44096667ca9afc41908780b392239ef3a5
Robert Kleinert <robert.kleinert@snk-interactive.de>
2015-02-19 08:41:12 +0100

    updated styles

---
+++ docs/template/public/main.js
@@ -0,0 +1,6 @@
+ var
+    $menu = $('.kss-menu'),
+    $childMenu = $('.kss-menu-child'),
+    $menuItem = $menu.find('.kss-menu-item'),
+    $childMenuItem = $childMenu.find('.kss-menu-item'),
+    ref = $menu.data('kss-ref');
