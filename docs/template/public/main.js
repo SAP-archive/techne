189c59a0b918caf61f0a13d8be5f513b95985537
Robert Kleinert <robert.kleinert@snk-interactive.de>
2015-02-19 10:18:20 +0100

    style update

--- docs/template/public/main.js
+++ docs/template/public/main.js
@@ -1,6 +1,11 @@
  var
-    $menu = $('.kss-menu'),
+    $menu = jQuery('.kss-menu'),
     $childMenu = $('.kss-menu-child'),
     $menuItem = $menu.find('.kss-menu-item'),
     $childMenuItem = $childMenu.find('.kss-menu-item'),
     ref = $menu.data('kss-ref');
+    console.log(ref);
+ jQuery(document).ready(function() {
+     console.log('foo');
+     $menuItem.eq(ref).addClass('kss-active');
+ });
