3e3bd7a3ead822b2e110c2fd1e39a6cb6c15ac8c
Jeremy Rawls <jeremy.rawls@hybris.com>
2015-04-16 22:40:57 -0500

    adjusting markup for data cards

--- src/js/addToList.js
+++ src/js/addToList.js
@@ -2,9 +2,7 @@ $(document).ready(function(){
     $(document).on('click', '.y_addTolistHandle', function(e){
 
         e.preventDefault();
-        $(e.currentTarget).parents('.y_addToListContainer').find('.y_content').toggleClass('active');
-
-        $(e.currentTarget).toggleClass('active')
+        $(e.currentTarget).parents('.y_addToListContainer').find('.panel-heading').toggleClass('active')
 
     });
 });
