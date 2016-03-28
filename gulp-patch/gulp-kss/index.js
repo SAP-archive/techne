4e90248830a64c9becf8e9234ea6bc7a08dfb79d
HybrisRawls <jeremy.rawls@hybris.com>
2016-03-28 17:50:21 -0500

    initial commit for the correct naming of the generated component files

--- gulp-patch/gulp-kss/index.js
+++ gulp-patch/gulp-kss/index.js
@@ -51,6 +51,7 @@ module.exports = function(opt) {
                     rootCount, childSections = [];
 
 
+
             // Accumulate all of the sections' first indexes
             // in case they don't have a root element.
             for (i = 0; i < sectionCount; i += 1) {
@@ -63,7 +64,9 @@ module.exports = function(opt) {
 
             sectionRoots.sort();
             rootCount = sectionRoots.length;
+            
 
+            //console.log(styleguide);
             handlebarHelpers(handlebars, styleguide);
 
             // Generate HTML from all supplied markdown files
@@ -117,7 +120,24 @@ module.exports = function(opt) {
             // Now, group all of the sections by their root
             // reference, and make a page for each.
             for (i = 0; i < rootCount; i += 1) {
+
+                //console.log('sc', styleguide.section);
                 childSections = styleguide.section(sectionRoots[i]+'.*');
+
+                console.log(styleguide.data[i]);
+                
+                var fileRoot = parseInt(sectionRoots[i],10);
+
+
+                console.log('froot', fileRoot);
+
+                var fileName = styleguide.data.section_refs[fileRoot].data.header.replace(/[^a-zA-Z0-9]/g,'-').replace(/-+/g,'-').replace(/\-$/, "");
+
+
+                //update the childSections reference to point at the new file name links
+                //console.log(styleguide.data.section_refs);
+                childSections.pageLink = fileName;
+                //console.log(sectionRoots.pageLink);
 
                 var content = template({
                     showLeftNav: true, //show the nav bar for all sections
@@ -130,7 +150,10 @@ module.exports = function(opt) {
                     argv: {}
                 });
 
-                var joinedPath = path.join(firstFile.base, 'section-' + sectionRoots[i] + '.html');
+                //console.log('var index' + i + ' = ', childSections);
+
+                var joinedPath = path.join(firstFile.base,  fileName + '.html');
+
 
                 var file = new File({
                     cwd: firstFile.cwd,
@@ -161,6 +184,7 @@ module.exports = function(opt) {
     }
 
     function jsonSections(sections) {
+
         return sections.map(function(section) {
             return {
                 header: section.header(),
