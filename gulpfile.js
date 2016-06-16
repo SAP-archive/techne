1ef76822e184b4439d8c9ebb92ab4c255fbc6b39
HybrisRawls <jeremy.rawls@hybris.com>
2016-06-16 14:16:30 -0500

    updating templates and remove pagination

--- gulpfile.js
+++ gulpfile.js
@@ -283,7 +283,14 @@ gulp.task('patchgulpkss',
 gulp.task('starterpages',
     function(){
         //copy starter pages files to kss folder
-        gulp.src('docs/starter-pages/**/*').pipe(gulp.dest('docs/kss/starter-pages'));
+        gulp.src(['docs/starter-pages/css/**/*',
+                  'docs/starter-pages/images/**/*',
+                  'docs/starter-pages/cards.html',
+                  'docs/starter-pages/details-1.html',
+                  'docs/starter-pages/details-2.html',
+                  'docs/starter-pages/table.html',
+                  'docs/starter-pages/index.html'],
+                  {base: 'docs/'  }).pipe(gulp.dest('docs/kss'));
     }
 );
 
