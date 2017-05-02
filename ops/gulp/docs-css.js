3a60c43cbffa21dfef7cea3fd5a5bdcd5a70388a
Mohiuddin, Saad <saad.mohiuddin@sap.com>
2017-05-02 14:33:46 -0500

    638: updated css gulp taks to include a task for compiling and copying over styleguide styles

--- ops/gulp/docs-css.js
+++ ops/gulp/docs-css.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp')
 const gulpSequence    = require('gulp-sequence')
+const sass = require('gulp-sass')
 
 const config = require('../config')
 let environment = require('../lib/environment')
@@ -7,22 +8,20 @@ let environment = require('../lib/environment')
 const debug = require('gulp-debug');
 
 const task = (cb) => {
+	techneCss()
+	techneGuidelines()
+}	
 
-
-	//assets are served from dist file when running locally
-	//if(environment.production) {
-
-	  return gulp.src(['./dist/css/techne-all.css','./dist/css/techne-debug.css'])
-		  //.pipe(debug())
-		  .pipe(gulp.dest('./www/css'));
-
-	//}
-	
-	
-
+const techneCss = (cb) => {
+ 	return gulp.src(['./dist/css/techne-all.css','./dist/css/techne-debug.css'])
+		.pipe(gulp.dest('./www/css'));
 }
 
-
+const techneGuidelines = (cb) => {
+	return gulp.src('./docs/styleguide/styles/*.scss')
+    	.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+    	.pipe(gulp.dest('./www/css'));	
+}
 
 gulp.task('docs-css', ['css'], task)
 module.exports = task
