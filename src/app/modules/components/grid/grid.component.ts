db376dfae1821d8faaf1779d6f78bd65a75692ce
Jose E. Gonzalez Modecir <jose.enrique.gonzalez.modecir@sap.com>
2017-05-17 15:38:02 +0200

    Horizontal grid width now working

--- src/app/modules/components/grid/grid.component.ts
+++ src/app/modules/components/grid/grid.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'tn-grid',
-  templateUrl: './grid.component.html'
+    selector: 'tn-grid',
+    templateUrl: './grid.component.html'
 })
 export class GridComponent {
+    gridColumns: number = 10;
+    gridRows: number = 10;
+    gridCells = [];
+
+    constructor() {
+        for (var i = 0; i < 100; i++) {
+            this.gridCells.push(
+                { type: 'empty', gridIndex: i  }
+            );
+        }
+    }
+
 }
+
