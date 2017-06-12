9637c0d8688a2a056ee31d537694e23f47be041d
Uliana <uliana.andriieshyna@hybris.com>
2017-06-12 17:37:33 +0200

    CCE-639, first implementation of respomsive  primary and secondary nav, left nav and footer

--- docs/styleguide/scripts/main.js
+++ docs/styleguide/scripts/main.js
@@ -1,5 +1,13 @@
-/**
- * Created by d063436 on 5/21/17.
- */
-console.log('Just for js test purpose');
 
+$(document).ready(function(){
+    var primaryNavigationDropdown = $('.yaas-signature-nav__list');
+    $('.primary-nav--toggle').on('click', function(){
+        $(this).toggleClass('active');
+        $(primaryNavigationDropdown).toggleClass('open');
+    });
+
+    $('#menu-toggle').on('click', function(){
+        $(this).toggleClass('open');
+        $('.mobile-left-side-navigation').toggleClass('open');
+    })
+});
