53d1ec366ac97b252adf1a033ab5cfc1febbfec0
Zack <zack.frazier@sap.com>
2017-06-14 18:33:17 -0500

    #683 adds component templates

--- ops/gulp/create.js
+++ ops/gulp/create.js
@@ -1,12 +1,23 @@
 const gulp   = require('gulp')
 const del    = require('del')
 const path   = require('path')
-const config = require('../config')
+const rename   = require('gulp-rename')
+const gulpreplace   = require('gulp-replace')
 const yargs = require('yargs');
+const util = require('../lib/util');
 
 const fs = require('fs');
 
-const componentName = yargs.argv.component;
+const componentId = yargs.argv.component;
+const pageId = yargs.argv.page;
+
+const paths = {
+	src: './ops/misc',
+    dest: {
+        lib: './src',
+        docs: './docs'
+    }
+}
 
 const paths = {
 	src: './src'
@@ -14,6 +25,7 @@ const paths = {
 
 const createTask = (cb) => {
 
+<<<<<<< HEAD
   if (!componentName) {
     return;
   }
@@ -29,7 +41,51 @@ const createTask = (cb) => {
   fs.writeFile(`${paths.src}/templates/${componentName}.html`, `{% set ${componentName} = components.${componentName} %}\n{{ ${componentName} }}`, function (err) {
     if (err) return console.log(err);
   });
+=======
+    if (!componentId) {
+        return;
+    }
+    let componentName = util.namerize(componentId);
+    let componentMethod = util.methodize(componentId);
 
+    //scss
+    gulp.src(`${paths.src}/src-tmpl.scss`)
+        .pipe(rename(`${componentId}.scss`))
+        .pipe(gulpreplace('__COMPONENT_ID__',componentId))
+        .pipe(gulp.dest(`${paths.dest.lib}/styles/components`));
+
+    //json
+    gulp.src(`${paths.src}/src-tmpl.json`)
+        .pipe(rename(`${componentId}.json`))
+        .pipe(gulpreplace('__COMPONENT_ID__',componentId))
+        .pipe(gulpreplace('__COMPONENT_NAME__',componentName))
+        .pipe(gulp.dest(`${paths.dest.lib}/data`));
+
+    //html
+    gulp.src(`${paths.src}/src-tmpl.html`)
+        .pipe(rename(`${componentId}.html`))
+        .pipe(gulpreplace('__COMPONENT_ID__',componentId))
+        .pipe(gulpreplace('__COMPONENT_METHOD__',componentMethod))
+        .pipe(gulp.dest(`${paths.dest.lib}/templates`));
+
+    if (!pageId) {
+        return;
+    }
+    let pageName = util.namerize(pageId);
+    //html
+    gulp.src(`${paths.src}/docs-tmpl.html`)
+        .pipe(rename(`${pageId}.html`))
+        .pipe(gulpreplace('__COMPONENT_ID__',componentId))
+        .pipe(gulpreplace('__COMPONENT_NAME__',componentName))
+        .pipe(gulpreplace('__COMPONENT_METHOD__',componentMethod))
+        .pipe(gulp.dest(`${paths.dest.docs}/styleguide/components`));
+>>>>>>> #683 adds component templates
+
+    //json
+    gulp.src(`${paths.src}/docs-tmpl.json`)
+        .pipe(rename(`${pageId}.json`))
+        .pipe(gulpreplace('__PAGE_NAME__',pageName))
+        .pipe(gulp.dest(`${paths.dest.docs}/data`));
 
 }
 
