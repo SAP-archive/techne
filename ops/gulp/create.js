d8481dbc86c5694f366fe9fc90d58c9056015422
Zack <zack.frazier@sap.com>
2017-06-14 19:14:05 -0500

    fixes after testing

--- ops/gulp/create.js
+++ ops/gulp/create.js
@@ -1,8 +1,7 @@
-const gulp   = require('gulp')
-const del    = require('del')
-const path   = require('path')
-const rename   = require('gulp-rename')
-const gulpreplace   = require('gulp-replace')
+const gulp = require('gulp')
+const rename = require('gulp-rename')
+const replace = require('gulp-replace')
+const footer = require('gulp-footer')
 const yargs = require('yargs');
 const util = require('../lib/util');
 
@@ -34,21 +33,25 @@ const createTask = (cb) => {
     //scss
     gulp.src(`${paths.src}/src-tmpl.scss`)
         .pipe(rename(`${componentId}.scss`))
-        .pipe(gulpreplace('__COMPONENT_ID__',componentId))
+        .pipe(replace('__COMPONENT_ID__',componentId))
         .pipe(gulp.dest(`${paths.dest.lib}/styles/components`));
+
+    gulp.src(`${paths.dest.lib}/styles/components.scss`)
+        .pipe(footer(`@import "components/${componentId}";\n`))
+        .pipe(gulp.dest(`${paths.dest.lib}/styles`));
 
     //json
     gulp.src(`${paths.src}/src-tmpl.json`)
         .pipe(rename(`${componentId}.json`))
-        .pipe(gulpreplace('__COMPONENT_ID__',componentId))
-        .pipe(gulpreplace('__COMPONENT_NAME__',componentName))
+        .pipe(replace('__COMPONENT_ID__',componentId))
+        .pipe(replace('__COMPONENT_NAME__',componentName))
         .pipe(gulp.dest(`${paths.dest.lib}/data`));
 
     //html
     gulp.src(`${paths.src}/src-tmpl.html`)
         .pipe(rename(`${componentId}.html`))
-        .pipe(gulpreplace('__COMPONENT_ID__',componentId))
-        .pipe(gulpreplace('__COMPONENT_METHOD__',componentMethod))
+        .pipe(replace('__COMPONENT_ID__',componentId))
+        .pipe(replace('__COMPONENT_METHOD__',componentMethod))
         .pipe(gulp.dest(`${paths.dest.lib}/templates`));
 
     if (!pageId) {
@@ -58,15 +61,15 @@ const createTask = (cb) => {
     //html
     gulp.src(`${paths.src}/docs-tmpl.html`)
         .pipe(rename(`${pageId}.html`))
-        .pipe(gulpreplace('__COMPONENT_ID__',componentId))
-        .pipe(gulpreplace('__COMPONENT_NAME__',componentName))
-        .pipe(gulpreplace('__COMPONENT_METHOD__',componentMethod))
-        .pipe(gulp.dest(`${paths.dest.docs}/styleguide/components`));
+        .pipe(replace('__COMPONENT_ID__',componentId))
+        .pipe(replace('__COMPONENT_NAME__',componentName))
+        .pipe(replace('__COMPONENT_METHOD__',componentMethod))
+        .pipe(gulp.dest(`${paths.dest.docs}/styleguide`));
 
     //json
     gulp.src(`${paths.src}/docs-tmpl.json`)
         .pipe(rename(`${pageId}.json`))
-        .pipe(gulpreplace('__PAGE_NAME__',pageName))
+        .pipe(replace('__PAGE_NAME__',pageName))
         .pipe(gulp.dest(`${paths.dest.docs}/data`));
 
 }
