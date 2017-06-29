9c8adda0c9e56d79c7115b118a5f908bc91065c9
Gornicki, Lukasz <lukasz.gornicki@hybris.com>
2017-06-29 17:29:16 +0200

    updated gulp deploy
    after feedback changes the project structure in ghpages

--- ops/gulp/ghpages-deploy.js
+++ ops/gulp/ghpages-deploy.js
@@ -1,9 +1,36 @@
-var gulp = require('gulp');
-var ghPages = require('gulp-gh-pages');
+const gulp = require('gulp');
+const ghPages = require('gulp-gh-pages');
+const gulpSequence = require('gulp-sequence');
+
+const paths = {
+    srcPrepare:  ['./www/**/*','./README.md'],
+    destPrepare: './ghpages',
+    srcDeploy:   './ghpages/**/*'
+}
+
+//main task that should be used
+const task = (cb) => {
+
+    gulpSequence('prepareDeploy',['ghDeploy'], cb);
+
+}
+
+//Task to prepare folder with www and readme that are be deployed to ghpages
+const prepareDeploy = (cb) => {
+
+    return gulp.src(paths.srcPrepare,{ base: './' })
+      .pipe(gulp.dest(paths.destPrepare));
+
+
+}
 
 //Task for deploying compiled HTML to gh-pages branch for GitHub Pages hosting
-gulp.task('deploy', function() {
-    
-  return gulp.src('./www/**/*')
-    .pipe(ghPages({remoteUrl:'git@github.com:SAP/techne.git'}));
-});
+const ghDeploy = (cb) => {
+
+    return gulp.src(paths.srcDeploy)
+      .pipe(ghPages({remoteUrl:'git@github.com:SAP/techne.git'}));
+}
+
+gulp.task('prepareDeploy', prepareDeploy);
+gulp.task('ghDeploy', ghDeploy);
+gulp.task('deploy', task);
