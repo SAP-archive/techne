e48feeda8922fe331b29e08ec6f9998eb553b6f7
Mohiuddin, Saad <saad.mohiuddin@sap.com>
2017-07-13 17:02:07 -0500

    723: updated docs-resources to copy over imgs

--- ops/gulp/docs-resources.js
+++ ops/gulp/docs-resources.js
@@ -2,13 +2,13 @@ const gulp = require('gulp')
 const imagemin = require('gulp-imagemin')
 
 const task = (cb) => {
-  return gulp.src('./docs/resources/svgs/**/*.svg')
+  return gulp.src('./docs/resources/**/*')
     .pipe(imagemin(
       [imagemin.svgo({
           plugins: [{removeTitle: true}, {cleanupIDs: true}]
       })]
     ))
-    .pipe(gulp.dest('./www/svg/'));
+    .pipe(gulp.dest('./www/'));
 }
 
 gulp.task('docs-resources', task)
