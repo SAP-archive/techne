24828ff494b80bfa1b19db42fe255d73fffa73b1
Zack <zack.frazier@sap.com>
2017-08-07 15:17:12 -0500

    rough of test html page

--- test/app.js
+++ test/app.js
@@ -1,10 +1,41 @@
+var nunjucks = require('nunjucks');
 var express = require('express');
 var path = require('path');
 var app = express();
 var router = express.Router();
+var sass = require('node-sass');
 
-app.set('views', path.join(__dirname, 'templates'));
-app.set('view engine', 'pug');
+var TEMPLATE_DIRECTORY = path.join(__dirname, 'templates');
+var CSS_DIRECTORY = path.join(__dirname, 'public');
+
+var SASS_DIRECTORY = "../scss/components";
+
+var sassToCss = function(sassFile="app.scss") {
+    var scss_filename = `${CSS_DIRECTORY}/${sassFile}`;
+    return sass.renderSync({
+        file: scss_filename
+    }).css.toString();
+
+}
+console.log(sassToCss());
+
+
+// looks for html in views folder relative to current working directory
+var env = nunjucks.configure([TEMPLATE_DIRECTORY,CSS_DIRECTORY], {
+    autoescape: true,
+    cache: false,
+    express: app,
+    watch: true
+});
+env.addFilter('sassToCss', function(sassFile="app.scss") {
+    var scss_filename = `${SASS_DIRECTORY}/${sassFile}`;
+    return sass.renderSync({
+        file: scss_filename
+    }).css.toString();
+});
+
+app.set('views', TEMPLATE_DIRECTORY);
+app.set('view engine', 'njk');
 
 app.use(router);
 app.use(express.static(path.join(__dirname, 'public')));
