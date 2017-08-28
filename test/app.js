152b378f3a8983a4f464f8fce0053aea5280e7f4
Zack <zack.frazier@sap.com>
2017-08-28 11:34:13 -0500

    #921 fixed app.js route

--- test/app.js
+++ test/app.js
@@ -95,7 +95,7 @@ app.use('/static', express.static(path.join(__dirname, 'resources')));
 
 //load font files
 router.get('/TechneIcons:key', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', `scss/icons/hyicon${req.params.key}`));
+    res.sendFile(path.join(__dirname, '..', `scss/icons/TechneIcons${req.params.key}`));
 });
 
 router.all('/', function (req, res, next) {
