41097aa95b0b3a85e097f62a873cd732777b6cbf
Lukasz Gornicki <derberg@wp.pl>
2017-11-27 16:36:31 +0100

    #1204

--- ops/gulp/ghpages-deploy.js
+++ ops/gulp/ghpages-deploy.js
@@ -28,7 +28,7 @@ const prepareDeploy = (cb) => {
 //Task for deploying compiled HTML to gh-pages branch for GitHub Pages hosting
 const ghDeploy = (cb) => {
 
-    ghPages.publish(paths.srcDeploy, cb);
+    ghPages.publish(paths.destPrepare, cb);
     //return gulp.src(paths.srcDeploy)
     //  .pipe(ghPages());
 }
