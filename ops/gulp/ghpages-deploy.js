7049bda8ff7cb4171d1fd3e396ff938e6913f93a
Lukasz Gornicki <derberg@wp.pl>
2017-11-27 15:50:00 +0100

    #1204

--- ops/gulp/ghpages-deploy.js
+++ ops/gulp/ghpages-deploy.js
@@ -28,7 +28,7 @@ const prepareDeploy = (cb) => {
 const ghDeploy = (cb) => {
 
     return gulp.src(paths.srcDeploy)
-      .pipe(ghPages({remoteUrl:'git@github.com:SAP/techne.git'}));
+      .pipe(ghPages());
 }
 
 gulp.task('prepareDeploy', prepareDeploy);
