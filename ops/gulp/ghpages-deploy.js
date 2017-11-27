7ef736c27175bee051f436276ff3192344938a86
Lukasz Gornicki <derberg@wp.pl>
2017-11-27 14:03:20 +0100

    #1204 revert

--- ops/gulp/ghpages-deploy.js
+++ ops/gulp/ghpages-deploy.js
@@ -28,7 +28,7 @@ const prepareDeploy = (cb) => {
 const ghDeploy = (cb) => {
 
     return gulp.src(paths.srcDeploy)
-      .pipe(ghPages({branch:'gh-pages'}));
+      .pipe(ghPages());
 }
 
 gulp.task('prepareDeploy', prepareDeploy);
